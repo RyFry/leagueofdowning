51fd9032767949c3ee7db2561141801279adc5e6
Nathan Williamson <npw1990@utexas.edu>
2015-07-06 16:29:32 -0500

    add players.html, remove roles.html

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 angular.module('app', [])
 
-.controller('roleController', function($scope) {
+.controller('playerController', function($scope) {
 	$scope.sortType = 'name';
 	$scope.sortReverse = false;
 
-	$scope.roles = [
-		{name: 'foo', champions: 'xyz'},
-		{name: 'bar', champions: 'zyx'}
+	$scope.players = [
+
+		{name: 'Bjergsen', age: 19, position: "mid", totalWins: 9, seasonWins: 9, 
+			seasonLosses: 3, team: "TSM", avgKDA: 4.7, avgGPM: 308, avgTotalGold: 18000,
+			champions: ['Azir', 'Ezreal', 'Ekko']},
+
+		{name: 'Doublelift', age: 21, position: "adc", totalWins: 7, seasonWins: 7, 
+			seasonLosses: 5, team: "CLG", avgKDA: 4.1, avgGPM: 426, avgTotalGold: 16000,
+			champions: ['Ashe', 'Sivir', 'Kalista']},
+
+		{name: 'Balls', age: 21, position: "top", totalWins: 3, seasonWins: 3, 
+			seasonLosses: 9, team: "C9", avgKDA: 1.8, avgGPM: 301, avgTotalGold: 11000,
+			champions: ['Fizz', 'Maokai', 'Rumble']}
 	];
 });
