fddbd4c9f655195ca6917cd2d8653f5a01bb3ef9
Ryan <ryanlorey@outlook.com>
2015-07-07 00:19:32 +0000

    Added champions pages for Azir and Ezreal

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
     url(r'^test', views.test, name='test'),
-    url(r'^champions/dr_mundo', views.mundo, name='dr_mundo'),
     url(r'^players', views.players, name='players'),
-    url(r'^items', views.items, name='items')
+    url(r'^items', views.items, name='items'),
+
+    url(r'^champions', views.champions, name='champions'),
+    url(r'^champions/dr_mundo', views.mundo, name='dr_mundo'),
+    url(r'^champions/azir', views.azir, name='azir'),
+    url(r'^champions/ezreal', views.ezreal, name='ezreal'),
 ]
--- app/views.py
+++ app/views.py
@@ -47,8 +47,59 @@ def mundo(request):
     })
     return HttpResponse(template.render(context))
 
+def azir(request):
+    template = loader.get_template('app/champions/champion.html')
+    context = RequestContext(request, {
+        'champion_name' : "Azir",
+        'image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/champion/Azir.png",
+        'passive_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/passive/Azir_Passive.png",
+        'q_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/AzirQ.png",
+        'w_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/AzirW.png",
+        'e_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/AzirE.png",
+        'r_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/AzirR.png",
+        'passive' : "Shurima's Legacy",
+        'passive_description' : "Azir can summon the Disc of the Sun from the ruins of allied or enemy turrets. Additionally, Azir gains Attack Speed based on the amount of Cooldown Reduction he has.",
+        'q' : "Conquering Sands",
+        'q_description' : "Azir sends all Sand Soldiers toward a target location. Sand Soldiers deal magic damage to all targets they pass through and apply a stacking slow for 1 second.\n\nAzir sends all Sand Soldiers toward a target location. Sand Soldiers deal 65/85/105/125/145 (+50% Ability Power) magic damage to all enemies they pass through and apply a stacking 25% slow for 1 second.",
+        'w' : "Arise!",
+        'w_description' : "Azir summons a Sand Soldier to attack nearby targets for him, replacing his basic attack against targets within the soldier's range. Their attacks deal magic damage to enemies in a line.\n\nAzir summons a Sand Soldier for 9 seconds. When Azir attacks an enemy in a soldier's range, the soldier attacks instead of Azir, dealing 0 (+60% Ability Power) magic damage to enemies in a line. If multiple soldiers strike the same target, each soldier after the first deals 25% damage. Soldiers attack even if Azir himself isn't in basic attack range.Azir can store up to {{ maxammo }} Sand Soldiers at a time. A new soldier becomes available every 0 seconds. Moving too far away from soldiers deactivates them. Sand Soldiers expire twice as fast when near an enemy turret.Azir can expend a soldier to damage an enemy turret by summoning it on top of the turret. This deals 0 (+40% Ability Power) magic damage to the turret.",
+        'e' : "Shifting Sands",
+        'e_description' : "Azir dashes to one of his Sand Soldiers, knocking up and damaging enemies. If he hits an enemy champion, he gains a shield.\n\nAzir dashes to one of his Sand Soldiers, damaging enemies hit for 60/90/120/150/180 (+40% Ability Power) magic damage and knocking them up for 0.5 seconds. If Azir hits an enemy champion, he stops and gains a 4 second shield that absorbs 80/120/160/200/240 (+0) damage.",
+        'r' : "Emperor's Divide",
+        'r_description' : "Azir summons a wall of soldiers which charge forward, knocking back and damaging enemies.\n\nAzir summons a wall of armored soldiers that charge forward, knocking back enemies and dealing 150/225/300 (+60% Ability Power) magic damage. The soldiers then remain as a wall for 5/6/7 seconds.Enemies will be stopped by Emperor's Divide, even if they attempt to dash over the wall, but Azir and his allies can pass freely and gain 20% Movement Speed for 1 second when they pass through the wall.Emperor's Divide does not interact with Azir's basic attacks or spells.",
+    })
+    return HttpResponse(template.render(context))
+
+def ezreal(request):
+    template = loader.get_template('app/champions/champion.html')
+    context = RequestContext(request, {
+        'champion_name' : "Ezreal",
+        'image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/champion/Ezreal.png",
+        'passive_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/passive/Ezreal_RisingSpellForce.png",
+        'q_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/EzrealMysticShot.png",
+        'w_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/EzrealEssenceFlux.png",
+        'e_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/EzrealArcaneShift.png",
+        'r_image' : "http://ddragon.leagueoflegends.com/cdn/5.10.1/img/spell/EzrealTrueshotBarrage.png",
+        'passive' : "Rising Spell Force",
+        'passive_description' : "Hitting a target with any of Ezreal's abilities increases his Attack Speed by 10% for 6 seconds (effect stacks up to 5 times). ",
+        'q' : "Mystic Shot",
+        'q_description' : "Ezreal fires a damaging bolt of energy which reduces all of his cooldowns by 1 second if it strikes an enemy unit.\n\nEzreal fires a bolt of energy, dealing 35/55/75/95/115 (+0) (+40% Ability Power) physical damage (applies on-hit effects). Ezreal's cooldowns are reduced by 1 second if Mystic Shot hits a target.",
+        'w' : "Essence Flux",
+        'w_description' : "Ezreal fires a fluctuating wave of energy, dealing magic damage to enemy champions, while increasing the Attack Speed of allied champions.\n\nEzreal fires a wave of energy that damages all enemy champions it passes through for 70/115/160/205/250 (+80% Ability Power) magic damage. If Ezreal or his Allied champions are hit by the wave, their Attack Speed is increased by 20/25/30/35/40% for 5 seconds.",
+        'e' : "Arcane Shift",
+        'e_description' : "Ezreal teleports to a target nearby location and fires a homing bolt which strikes the nearest enemy unit.\n\nEzreal teleports to a target nearby location and fires a homing bolt at the nearest enemy unit, dealing 75/125/175/225/275 (+75% Ability Power) magic damage.",
+        'r' : "Trueshot Barrage",
+        'r_description' : "Ezreal winds up for 1 second to fire a powerful barrage of energy missiles which do massive damage to each unit they pass through (deals 10% less damage to each unit it passes through).\n\nEzreal winds up for 1 second to fire a barrage of missiles dealing 350/500/650 (+100% bonus Attack Damage) (+90% Ability Power) magic damage to each unit it passes through. However, it deals 10% less damage for each unit it hits (minimum 30%).",
+    })
+    return HttpResponse(template.render(context))
+
 def test(request):
     template = loader.get_template('app/index.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
+
+def champions(request):
+    template = loader.get_template('app/champions.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
 
