94f0f2b08cdb4e2ed84a55de667be921d19c1f37
Michael Cantu <mc44696@cs.utexas.edu>
2015-07-07 12:51:44 -0500

    added shell for tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestModels (TestCase) :
+
+    def test_champions_1 (self) :
+        v = 
+        self.assertEqual(v, )
+
+    def test_champions_2 (self) :
+        v = 
+        self.assertEqual(v, )
+
+    def test_champions_3 (self) :
+        v = 
+        self.assertEqual(v, )
+
+    def test_players_1 (self) :
+        v = 
+        self.assertEqual(v, )
+
+    def test_players_2 (self) :
+        v = 
+        self.assertEqual(v, )
+
+    def test_players_3 (self) :
+        v = 
+        self.assertEqual(v, )  
+
+    def test_items_1 (self) :
+        v = 
+        self.assertEqual(v, ) 
+
+    def test_items_2 (self) :
+        v = 
+        self.assertEqual(v, )   
+
+    def test_items_3 (self) :
+        v = 
+        self.assertEqual(v, )     
+    
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    main()
+
