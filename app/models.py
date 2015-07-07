144b62c471affb28b3e127acc8ef6571d9dc75d1
Ryan <ryanlorey@outlook.com>
2015-07-07 22:14:55 +0000

    Uncentered the pictures on about.html because it looked like crap on mobile

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.html import format_html
-
+from django.conf import settings
 
     # -------------
     # Champion_model
