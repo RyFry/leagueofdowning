c7ec77047dd036a6919500633b0edb627bb7e715
Nathan Williamson <npw1990@utexas.edu>
2015-07-13 16:13:31 -0500

    proper regex for dynamic pages (?)

--- app/urls.py
+++ app/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     url(r'^test', views.test, name='test'),
     
     url(r'^champions/$', views.champions),
-    url(r'^champions/.*', views.champion),
+    url(r'^champions/(?P<name>.*)', views.champion),
     url(r'^champions/*', views.champions),
     
     url(r'^items/$', views.items),
-    url(r'^items/.*', views.item),
+    url(r'^items/(?P<name>.*)', views.item),
     url(r'^items/*', views.items),
 
     url(r'^players/$', views.players),
-    url(r'^players/.*', views.player),
+    url(r'^players/(?P<name>.*)', views.player),
     url(r'^players/*', views.players),
 
     url(r'^.*/$', views.index)
