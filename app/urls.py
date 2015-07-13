ecccaa899b1ef1aa6329f1dd8b5ad55cf1a8b2d1
Nathan Williamson <npw1990@utexas.edu>
2015-07-13 15:55:56 -0500

    first try at dynamic page loading

--- app/urls.py
+++ app/urls.py
@@ -7,23 +7,17 @@ urlpatterns = [
     url(r'^about', views.about, name='about'),
     url(r'^test', views.test, name='test'),
     
-    url(r'^champions/$', views.champions, name='champions'),
-    url(r'^champions/dr_mundo', views.mundo, name='dr_mundo'),
-    url(r'^champions/azir', views.azir, name='azir'),
-    url(r'^champions/ezreal', views.ezreal, name='ezreal'),
-    url(r'^champions/*', views.champions, name='champions'),
+    url(r'^champions/$', views.champions),
+    url(r'^champions/.*', views.champion),
+    url(r'^champions/*', views.champions),
     
-    url(r'^items/$', views.items, name='items'),
-    url(r'^items/athenes_unholy_grail', views.athenes, name='athenes'),
-    url(r'^items/rabadons_deathcap', views.rabadons, name='rabadons'),
-    url(r'^items/sorcerers_shoes', views.sorc_shoes, name='sorc_shoes'),
-    url(r'^items/*', views.items, name='items'),
+    url(r'^items/$', views.items),
+    url(r'^items/.*', views.item),
+    url(r'^items/*', views.items),
 
-    url(r'^players/$', views.players, name='players'),
-    url(r'^players/balls', views.balls, name='balls'),
-    url(r'^players/bjergsen', views.bjergsen, name='bjergsen'),
-    url(r'^players/doublelift', views.doublelift, name='doublelift'),
-    url(r'^players/*', views.players, name='players'),
+    url(r'^players/$', views.players),
+    url(r'^players/.*', views.balls),
+    url(r'^players/*', views.players),
 
-    url(r'^.*/$', views.index, name='index')
+    url(r'^.*/$', views.index)
 ]
