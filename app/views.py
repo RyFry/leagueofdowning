620ee04bf8dcbb5cfef00da56143d533efb115b8
Nathan Williamson <npw1990@utexas.edu>
2015-07-14 16:33:53 -0500

    (tenth try)

--- app/views.py
+++ app/views.py
@@ -44,8 +44,10 @@ def items(request):
 
     result = engine.execute('select * from "Item"')
     List = {}
+    i = 0
     for row in result:
-        List[row['item_id']] = {'item_id': row['item_id'], 'name': row['name'], 'description': row['description'], 'base_gold': row['base_gold'], 'sell_gold': row['sell_gold'], 'total_gold': row['total_gold'], 'image': 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + row['image'][-8:]}
+        List[i] = {'item_id': row['item_id'], 'name': row['name'], 'description': row['description'], 'base_gold': row['base_gold'], 'sell_gold': row['sell_gold'], 'total_gold': row['total_gold'], 'image': 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + row['image'][-8:]}
+        i += 1
 
     jsonout = json.dumps(List)
     jsonout = jsonout[1:-1]
