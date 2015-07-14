7edf8ce646d87527bad27b5403122c3e054be454
Nathan Williamson <npw1990@utexas.edu>
2015-07-14 17:10:18 -0500

    (seventeenth try)

--- app/views.py
+++ app/views.py
@@ -49,10 +49,7 @@ def items(request):
 
     out = ""
     for item in List:
-        out += "{"
-        for k, v in item.items():
-            out = out + k + " : " + v + ", "
-        out = out[0:-2] + "}, "
+        out = out + json.dumps(item) + ", "
 
     out = out[0:-2]
 
