99a1b02828b9e968c523cb6d0496deea5a06a1a6
Michael Cantu <mc44696@cs.utexas.edu>
2015-07-14 14:29:59 -0500

    Nathans magic test

--- app/views.py
+++ app/views.py
@@ -127,9 +127,11 @@ def Item_List_API(request):
 
     session = Session()   
 
-    ItemObjects = session.query('* FROM "Item"').all()
+    Item1 = session.query(Item).filter(Item.item_id == 3266).one()
+   
+    jsonout = {'id': Item1.id, 'description': Item1.description}
 
-    return HttpResponse(json.dumps(ItemObjects), content_type="application/json")
+    return HttpResponse(json.dumps(jsonout), content_type="application/json")
 
 
 def Item_ID_API(request, id):
