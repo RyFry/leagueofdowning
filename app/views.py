8df5140a42671ca870247403680ec368535878bc
Michael Cantu <mc44696@cs.utexas.edu>
2015-07-17 16:47:20 -0500

    adding item to item

--- app/views.py
+++ app/views.py
@@ -116,7 +116,7 @@ def item(request, id):
 
             fromlist = []
             for row2 in fromresult:
-                fromlist.append(row1['item_id'])
+                fromlist.append(row2['item_id'])
 
             jsonout = {'item_id': row['item_id'], 'name': row['name'], 'description': row['description'], 'base_gold': row['base_gold'], 'sell_gold': row['sell_gold'], 'total_gold': row['total_gold'], 'image': 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + row['image'][-8:], 'from_items' : fromlist, 'into_items' : intolist}
 
@@ -324,7 +324,7 @@ def Item_ID_API(request, id):
 
             fromlist = []
             for row2 in fromresult:
-                fromlist.append(row1['item_id'])
+                fromlist.append(row2['item_id'])
 
 
             jsonout = {'item_id': row['item_id'], 'name': row['name'], 'description': row['description'], 'base_gold': row['base_gold'], 'sell_gold': row['sell_gold'], 'total_gold': row['total_gold'], 'image': 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + row['image'][-8:], 'from_items' : fromlist, 'into_items' : intolist}
