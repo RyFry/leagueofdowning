0ef4e490e5a5b2a14b5d5d9ed76e920f7337d4a6
Bao Nguyen <mannywrxnguyen@gmail.com>
2015-07-17 17:30:03 -0500

    update url and view.py

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
+    url(r'^search', views.search, name='search'),
     
     url(r'^champions/$', views.champions),
     url(r'^champions/(?P<id>.*)', views.champion),
