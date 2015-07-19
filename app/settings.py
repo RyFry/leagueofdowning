44460a364805a9c88120d4cd2ef820bc82e1f193
Michael Cantu <mc44696@cs.utexas.edu>
2015-07-19 13:54:03 -0500

    re adding haystack

--- app/settings.py
+++ app/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'leagueofdowning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sqlalchemy.engine',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'leagueofdowning',
         'USER': 'postgres',
         'PASSWORD': 'h1Ngx0',
