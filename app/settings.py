09a2239fd81e54736729e89f81990ad8b5066102
Michael Cantu <mc44696@cs.utexas.edu>
2015-07-19 11:34:44 -0500

    updated both settings

--- app/settings.py
+++ app/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+
+    'haystack',
+
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,13 +79,18 @@ WSGI_APPLICATION = 'leagueofdowning.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
 }
 
+# Haystack engine
 
+HAYSTACK_CONNECTIONS = {
+    'default' : {
+        'ENGINE' : 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL' : 'http://127.0.0.1:9200/',
+        'INDEX_NAME' : 'haystack',
+    },
+}
 
 
 # Internationalization
@@ -103,3 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "static")
+]
