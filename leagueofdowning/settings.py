30e09416f28c4cadc3d421b8720e3d8a13f9295a
Michael Cantu <mc44696@cs.utexas.edu>
2015-07-19 12:17:50 -0500

    switched settings

--- leagueofdowning/settings.py
+++ leagueofdowning/settings.py
@@ -34,7 +34,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+
+    'haystack',
+
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -75,6 +78,16 @@ WSGI_APPLICATION = 'leagueofdowning.wsgi.application'
 
 DATABASES = {
 
+}
+
+# Haystack engine
+
+HAYSTACK_CONNECTIONS = {
+    'default' : {
+        'ENGINE' : 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL' : 'http://127.0.0.1:9200/',
+        'INDEX_NAME' : 'haystack',
+    },
 }
 
 
